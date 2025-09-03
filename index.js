131945923e0c0c1057fa0828063471f301a96ac6
AbuBakkarSiddique007 <abubakkar.cce25.iiuc@gmail.com>
2025-09-04 03:02:54 +0600

    Update: carts post, get operation

--- index.js
+++ index.js
@@ -31,11 +31,35 @@ async function run() {
 
         const database = client.db("DineEasyDB");
         const menuCollection = database.collection("menu");
+        const reviewCollection = database.collection("reviews")
+        const cartCollection = database.collection("carts")
 
         app.get("/menu", async (req, res) => {
             const result = await menuCollection.find().toArray()
             res.send(result)
         })
+        app.get("/reviews", async (req, res) => {
+            const result = await reviewCollection.find().toArray()
+            res.send(result)
+        })
+
+
+        app.get("/carts", async (req, res) => {
+            const email = req.query.email
+            const query = { email: email }
+
+            const result = await cartCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.post("/carts", async (req, res) => {
+            const cartItem = req.body
+            const result = await cartCollection.insertOne(cartItem)
+            res.send(result)
+        })
+
+
+
 
 
 
